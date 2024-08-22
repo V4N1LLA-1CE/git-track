6115fcb7a1d69d23cced0af438d0ad3a63ea8484
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-23 01:52:45 +1000

    feat: hello command can now accept an option

--- src/main/java/com/github/GitTrack/commands/DemoCommands.java
+++ src/main/java/com/github/GitTrack/commands/DemoCommands.java
@@ -2,6 +2,7 @@ package com.github.GitTrack.commands;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 /**
  * DemoCommands
@@ -10,8 +11,8 @@ import org.springframework.shell.standard.ShellMethod;
 public class DemoCommands {
 
   @ShellMethod(key = "hello", value = "I will say hello")
-  public String hello() {
-    return "Hello World!";
+  public String hello(@ShellOption(defaultValue = "World") String arg) {
+    return "Hello " + arg + "!";
   }
 
   @ShellMethod(key = "bye", value = "I will say bye")
