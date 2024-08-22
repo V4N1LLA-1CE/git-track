1dcbaa5af336f7461343c98e6363cf5a8f54aab0
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-23 01:31:22 +1000

    first commit

---
+++ src/main/java/com/github/GitTrack/GitTrackApplication.java
@@ -0,0 +1,13 @@
+package com.github.GitTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/GitTrack/GitTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.GitTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
