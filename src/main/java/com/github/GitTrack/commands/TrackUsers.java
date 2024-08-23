d0edcf3a451c15399c7b2f5dc97b4557db864624
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-08-24 04:54:31 +1000

    feat: show command now displays user information from api

--- src/main/java/com/github/GitTrack/commands/TrackUsers.java
+++ src/main/java/com/github/GitTrack/commands/TrackUsers.java
@@ -4,22 +4,42 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import com.github.GitTrack.GitHubService;
+import com.github.GitTrack.GitHubUser;
+
+import reactor.core.publisher.Mono;
+
 /**
  * TrackUsers
  */
 @ShellComponent
 public class TrackUsers {
 
+  private final GitHubService gitHubService;
+
+  public TrackUsers(GitHubService service) {
+    this.gitHubService = service;
+  }
+
   @ShellMethod("List public user details of a GitHub user.")
   public String show(
-      @ShellOption(value = { "--user",
-          "-u" }, defaultValue = "", help = "The username of the GitHub user") String username) {
+      @ShellOption(defaultValue = "", help = "The username of the GitHub user") String username) {
 
-    if (username.trim().isEmpty()) {
-      return " Error: Username has not been provided.\n Example Usage: gt show -u <username>";
+    if (username.trim().isEmpty() || username == null) {
+      return " Error: Username has not been provided.\n Example Usage: gt show <username>";
     }
 
-    String res = String.format(" Hello %s", username);
-    return res;
+    // fetch user details
+    Mono<GitHubUser> userMono = gitHubService.getUser(username);
+
+    return userMono
+        .map(user -> String.format("Hello %s\nName: %s\nFollowers: %d\nFollowing: %d\nPublic Repos: %d",
+            user.login(),
+            user.name(),
+            user.followers(),
+            user.following(),
+            user.publicRepos()))
+        .onErrorReturn("Error: Unable to fetch user details.")
+        .block();
   }
 }
