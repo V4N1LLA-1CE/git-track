5f3b3a5308ccd63106883fc74ffc61af02ab8314
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-09-20 13:58:08 +1000

    feat: add github shell method

--- src/main/java/com/github/GitTrack/commands/TrackUsers.java
+++ src/main/java/com/github/GitTrack/commands/TrackUsers.java
@@ -14,6 +14,14 @@ public class TrackUsers {
 
   private final GitHubService gitHubService;
 
+  // ANSI color codes
+  private final String RED = "\u001B[31m";
+  private final String GREEN = "\u001B[32m";
+  private final String RESET = "\u001B[0m";
+  private final String BOLD = "\u001B[1m";
+  private final String BLUE = "\u001B[34m";
+
+  // auto inject service through constructor
   public TrackUsers(GitHubService service) {
     this.gitHubService = service;
   }
@@ -21,13 +29,6 @@ public class TrackUsers {
   @ShellMethod("List public user details of a GitHub user.")
   public String show(
       @ShellOption(defaultValue = "", help = "The username of the GitHub user") String username) {
-
-    // ANSI color codes
-    String RED = "\u001B[31m";
-    String GREEN = "\u001B[32m";
-    String RESET = "\u001B[0m";
-    String BOLD = "\u001B[1m";
-    String BLUE = "\u001B[34m";
 
     if (username.trim().isEmpty() || username == null) {
       return BOLD + RED + "Error: Username has not been provided." + RESET + BOLD + GREEN + "\n\nExample Usage: "
@@ -40,4 +41,16 @@ public class TrackUsers {
 
     return result;
   }
+
+  @ShellMethod("List recent github activities of a Github user.")
+  public String activity(@ShellOption(defaultValue = "", help = "The username of the GitHub user") String username) {
+    if (username.trim().isEmpty() || username == null) {
+      return BOLD + RED + "Error: Username has not been provided." + RESET + BOLD + GREEN + "\n\nExample Usage: "
+          + RESET + "gt activity" + BLUE + " <username>" + RESET + "\n";
+    }
+
+    // fetch activity details
+    return gitHubService.getActivity(username);
+  }
+
 }
