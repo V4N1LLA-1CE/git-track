2fbfc85a40c41699e19a2b3545b94f683e8dfb01
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-09-20 13:57:24 +1000

    feat: implement getActivity command

--- src/main/java/com/github/GitTrack/GitHubService.java
+++ src/main/java/com/github/GitTrack/GitHubService.java
@@ -4,7 +4,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,7 @@ public class GitHubService {
 
   private final com.github.GitTrack.Formatter formatter;
 
+  // Auto-inject formatter through constructor injection
   public GitHubService(Formatter formatter) {
     this.formatter = formatter;
   }
@@ -35,20 +39,51 @@ public class GitHubService {
 
       // Include check for failed response status code
       int statusCode = response.statusCode();
-      if (statusCode == 404) {
+      if (statusCode != 200) {
         return (String.format("User %s could not be found...", username));
       }
 
       // Parse JSON response for the body(String) and create a new JSONObject with it
       String responseBody = response.body();
-      JSONObject jsonObject = new JSONObject(responseBody);
+      JSONObject responseJson = new JSONObject(responseBody);
 
       // Extract and format data from JSON
-      return this.formatter.formatUserDetails(jsonObject);
+      return this.formatter.formatUserDetails(responseJson);
 
     } catch (Exception e) {
       e.printStackTrace();
-      return "Error occurred";
+      return "Error occurred.";
+    }
+  }
+
+  public String getActivity(String username) {
+    try {
+      // Create request template for https://api.github.com/users/{username}/events
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create(baseUrl + "/users/" + username + "/events?per_page=30"))
+          .header("accept", "application/vnd.github+json")
+          .build();
+
+      // Receive response body after sending request
+      HttpResponse<String> response = HttpClient.newHttpClient()
+          .send(request, HttpResponse.BodyHandlers.ofString());
+
+      // Include check for failed response
+      int statusCode = response.statusCode();
+      if (statusCode != 200) {
+        return String.format("Events for %s could not be found...", username);
+      }
+
+      // Parse response body and turn it into a JSONArray
+      String responseBody = response.body();
+      JSONArray responseJsonArray = new JSONArray(responseBody);
+
+      // Extract and format data from JSON
+      return this.formatter.formatEvent(responseJsonArray);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "Error occurred.";
     }
   }
 }
